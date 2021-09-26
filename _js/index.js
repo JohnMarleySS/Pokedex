612d7ec85f3b2413d1662ef77545f8334e62e151
John Marley da Silva Santos <65204093+JohnMarleySS@users.noreply.github.com>
2021-09-26 12:36:40 -0300

    Add files via upload

--- _js/index.js
+++ _js/index.js
@@ -12,14 +12,17 @@ const fetchPokemon = () => {
 
             const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
                 const types = pokemon.types.map(typesInfo => typesInfo.type.name)
-                const habilidades = pokemon.abilities.map(habilidadeInfo => habilidadeInfo.ability.name)
-                console.log(pokemon)
+                const status = pokemon.stats.map(stsInfo => stsInfo.stat.name)
+                const valueStatus = pokemon.stats.map(valeInfo => valeInfo.base_stat)
                 accumulator += `
                 <div class="card">
-                    <h2>${pokemon.id}. ${pokemon.name} ${habilidades}</h2>
-                    <img class=${types[0]}"alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokemon.id}.png" />
+                    <h2>${pokemon.id}. ${pokemon.name}</h2>
+                    <img class=${types[0]}"alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" />
                     <h2>${types.join(' | ')}</h2>
-                </div>
+                   
+                ${status[0]}: ${valueStatus[0]}
+                 
+                    </div>   
                 `
                 return accumulator
             }, '')
